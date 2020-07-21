9130ad094accea0e7c166adfaf9a1831cc3f32d1
LEONARDINHO-DEV <leonardinho-dev@macbook-pro-de-leonardinho-dev.home>
2020-07-21 17:32:59 +0200

    crud 90%

--- Project/api/controllers/PersonnesCtrl.js
+++ Project/api/controllers/PersonnesCtrl.js
@@ -1,6 +1,9 @@
 const database = require("../models")
+const { static } = require("express")
 
 
+
+//Afficher toute les personnes qui son dans la base de données 
 class PersonnesCtrl {
     static async prendAllPeople(req, res) {
         try {
@@ -11,14 +14,89 @@ class PersonnesCtrl {
 
         } catch (err) {
             return res.status(500).json(err.message)
+        }
+
+    }
+
+
+    //Retrouver des personnes atravers leurs id 
+    static async takeOnePersonne(req, res) {
+        const { id } = req.params
+        try {
+            const onePersonne = await database.personnes.findOne({
+                where:
+                {
+                    id: Number(id)
+
+                }
+            })
+            return res.status(200).json(onePersonne)
+
+        } catch (error) {
+            return res.status(500).json(err.message)
+        }
+    }
+
+
+    //Création d'une nouvelle personne 
+    static async createPersonnes(req, res) {
+        //Ma méthode 
+        // const newPersonne ={nom,prenom,email}=req.body
+        //Tuto méthode 
+        const newPersonne = req.body
+        try {
+            const newPersonneCreate = await database.personnes.create(newPersonne)
+            return res.status(200).json(newPersonneCreate)
+        }
+
+        catch (error) {
+            return res.status(500).json(err.message)
+
+        }
+    }
+
+
+
+
+    //Modifier mon profile
+    static async modifierProfile(req, res) {
+        const { id } = req.params
+        const newInfos = req.body
+
+        try {
+            await database.personnes.update(newInfos, { where: { id: Number(id) } }
+            )
+            const profilModifier = await database.personnes.findOne({ where: { id: Number(id) } })
+            return res.status(200).json(profilModifier)
+        }
+
+        catch (error) {
+            return res.status(500).json(error.message)
 
         }
 
+    }
 
 
 
+    //effacer un registre
+    static async effacerProfile(req, res) {
+        const { id } = req.params
+
+        try {
+            await database.personnes.destroy({ where: { id: Number(id) } })
+            return res.status(200).json("ok")
+
+
+        }
+
+        catch (error) {
+            return res.status(500).json(error.message)
+
+        }
 
     }
+
 
 
 }
--- Project/api/routes/personnesRoute.js
+++ Project/api/routes/personnesRoute.js
@@ -2,10 +2,23 @@
 const express = require("express")
 const Router = express.Router();
 const PersonnesCtrl = require("../controllers/PersonnesCtrl");
-
-
 // const router = Router();
 
+
+
+//Afficher toute les personnes qui son dans la base de données 
 Router.get("/personnes", PersonnesCtrl.prendAllPeople)
 
-module.exports = Router;
+//Retrouver des personnes atravers leurs id 
+Router.get("/personnes/:id", PersonnesCtrl.takeOnePersonne)
+
+//Création d'une nouvelle personne 
+Router.post("/personnes", PersonnesCtrl.createPersonnes)
+
+//Modifier profile
+Router.put("/personnes/:id", PersonnesCtrl.modifierProfile)
+
+//effacer un registre
+Router.delete("/personnes/:id", PersonnesCtrl.effacerProfile)
+
+module.exports = Router
